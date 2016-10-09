b7fcb638fa8ea2a312313b8c84eb0249c914420e
Jinny Yan <jinnyyan@ccs.neu.edu>
2016-10-09 16:10:05 -0400

    Added JustGaze

---
+++ JustGaze/Assets/HoloToolkit/SpatialMapping/Prefabs/ShitPutter.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.VR.WSA.Input;
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+
+public class ShitPutter : MonoBehaviour
+{
+
+
+    public GestureRecognizer gestures;
+
+    public LayerMask layerMask = -1;
+
+    public GameObject menu;
+
+    public List<GameObject> sceneObjects;
+
+    public GameObject cursor;
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+        gestures = new GestureRecognizer();
+        gestures.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.DoubleTap);
+        gestures.TappedEvent += Gestures_TappedEvent;
+        gestures.StartCapturingGestures();
+    }
+
+    private void Gestures_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        print("Saw Tap");
+        placeObjectFromList();
+    }
+
+    void openMenu()
+    {
+        menu.SetActive(!menu.activeInHierarchy);
+    }
+
+    void Update()
+    {
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 5.0f, layerMask);
+        if (hit)
+        {
+            cursor.transform.position = hitInfo.point;
+        }
+    }
+
+    void placeObjectFromList()
+    {
+        print(sceneObjects.Count);
+        if (sceneObjects.Count > 0)
+        {
+            print("Placing Object");
+            RaycastHit hitInfo;
+            bool hit = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 5.0f, layerMask);
+            if (hit)
+            {
+                GameObject temp = (GameObject)Instantiate(sceneObjects[0], hitInfo.point, Quaternion.identity);
+                temp.transform.LookAt(Camera.main.transform.position);
+                temp.transform.up = Vector3.up;
+            }
+            sceneObjects.RemoveAt(0);
+        }
+    }
+
+
+}
---
+++ JustGaze/Assets/menu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+    GestureRecognizer gestures;
+
+	// Use this for initialization
+	void Start () {
+        gestures = new GestureRecognizer();
+        gestures.TappedEvent += Gestures_TappedEvent;
+        gestures.StartCapturingGestures();
+	}
+
+    private void Gestures_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        RaycastHit hitInfo;
+
+        bool hit = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 5.0f, -1);
+        if (hit)
+        {
+            if(hitInfo.collider.tag == "ski")
+            {
+                SceneManager.LoadScene("Lodge");
+            }
+            else if( hitInfo.collider.tag == "beach")
+            {
+                SceneManager.LoadScene("Beach");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 5.0f, -1);
+        if (hit)
+        {
+            this.transform.position = hitInfo.point;
+        }
+    }
+}
