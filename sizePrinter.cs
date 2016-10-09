0bb6f7c63fa827dc1623e66445365ee417631339
Yasmeen Roumie <yasmeen+g@protonmail.com>
2016-10-09 14:41:13 -0400

    Add files via upload

---
+++ sizePrinter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sizePrinter : MonoBehaviour {
+
+    public GameObject input;
+	// Use this for initialization
+	void Start () {
+        print(input.transform.lossyScale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
